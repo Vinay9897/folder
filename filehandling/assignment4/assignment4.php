2ca81155c72b8fdd927877e249d336b475ef60f9
Vinay Yadav <vinayyadav91190@gmail.com>
2022-05-13 16:47:34 +0530

    create the variable for storing data from input

--- filehandling/assignment4/assignment4.php
+++ filehandling/assignment4/assignment4.php
@@ -1,3 +1,15 @@
 <?php
-
+$md5 = $_POST['md5'];
+$number_format = $_POST['number_format'];
+$ord = $_POST['ord'];
+$rtrim = $_POST['rtrim'];
+$str_replace = $_POST['str_replace'];
+$str_len = $_POST['str_len'];
+$strcasecmp = $_POST['strcasecmp'];
+$strrpos = $_POST['strrpos'];
+$substr = $_POST['substr'];
+$strtolower = $_POST['strtolower'];
+$bintohex = $_POST['bintohex'];
+$chop = $_POST['chop'];
+echo $md;
 ?>
